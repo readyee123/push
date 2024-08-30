48293ce4485733292f519aa6f2e0bfcf47635281
Jacob <pleas@djkl.com>
2024-08-30 17:19:34 -0600

    Initial Commit

---
+++ github Exercice/github_ExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_ExerciceApp.swift
+//  github Exercice
+//
+//  Created by Jacob reshetnikov on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct github_ExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
